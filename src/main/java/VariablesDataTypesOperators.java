3096c9b698f1242911c696588da9423b403eb973
Сергей Осипов <mrfix1033@mail.ru>
2024-06-12 16:28:22 +0300

    VariablesDataTypesOperators.java added

---
+++ src/main/java/VariablesDataTypesOperators.java
@@ -0,0 +1,20 @@
+public class VariablesDataTypesOperators {
+
+    public static void main(String[] args) {
+        int integer = 14;
+        double double_number = 1.4;
+        char c = 61;
+        double result = integer + double_number + c;
+        System.out.println(result);
+
+        int a = 5, b = 12;
+        boolean check = a < b;
+        System.out.println(check);
+
+        a = 6;
+        a += 6;
+        a -= 2;
+        a /= 5;
+        a *= 7;
+    }
+}
