d67c209b8f65f4c3b8997099e59d8977f29ebf52
Сергей Осипов <mrfix1033@mail.ru>
2024-06-12 17:14:53 +0300

    Calculator.java added

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public double a, b;
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double sub(double a, double b) {
+        return a - b;
+    }
+
+    public void mul(double a, double b) {
+        System.out.println(a * b);
+    }
+
+    public void div(double a, double b) {
+        System.out.println(a / b);
+    }
+}
