c0787f3ce45f655cca873c492c694120e9bc63a8
Сергей Осипов <mrfix1033@mail.ru>
2024-06-28 11:59:47 +0300

    The homework "Sets" is updated.

--- src/main/java/Sets.java
+++ src/main/java/Sets.java
@@ -7,6 +7,9 @@ public class Sets {
         Set<String> firstSet = Set.of("green", "red", "blue", "cyan"), secondSet = Set.of("red", "yellow", "purple"), contained = new HashSet<>();
         getContainedInBoth(firstSet, secondSet, contained);
         System.out.println(contained);
+
+        Set<Integer> numbers = Set.of(1, 3, 2, 4, 16, 18, 17);
+        numbers.forEach(num -> System.out.println(num + " -> " + (num > 15 && (num & 1) == 0 ? num : num / 2)));
     }
 
     public static <cT extends Collection<T>, T> void getContainedInBoth(cT firstCollection, cT secondCollection, Collection<T> container) {
