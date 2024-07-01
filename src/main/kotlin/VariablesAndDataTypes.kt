9655093d1f7eb0f94bf81dce2ef67572fcf95175
Сергей Осипов <mrfix1033@mail.ru>
2024-07-01 20:34:33 +0300

    The homework "VariablesAndDataTypes" is completed.

---
+++ src/main/kotlin/VariablesAndDataTypes.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val age: Int = (1 shl 1 shl 1 shl 1 shl 1) + 1
+    val name: String = "Arseniy"
+    val isStudent: Boolean = true
+    val salary: Double = -1000.0
+    println("Name: " + name + " | Age: " + age + " | isStudent: " + isStudent + " | Salary: " + salary)
+}
